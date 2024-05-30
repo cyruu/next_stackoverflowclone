55bc28020478f266b2037af9060910a8774a06ad
cyruu <cyruz.mhr09@gmail.com>
2024-05-30 21:03:03 +0545

    questions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,11 @@
 "use client";
-import Navbar from "@/app/components/Navbar";
+
 import LandingPage from "@/app/components/LandingPage";
-import { ThemeProvider } from "@emotion/react";
-import theme from "@/materialUI/theme";
+
 export default function Home() {
   return (
     <>
-      <ThemeProvider theme={theme}>
-        <Navbar />
-        <LandingPage />
-      </ThemeProvider>
+      <LandingPage />
     </>
   );
 }
